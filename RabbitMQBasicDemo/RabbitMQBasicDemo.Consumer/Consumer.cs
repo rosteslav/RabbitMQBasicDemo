080f3197b5318a2f64e5034c527d4962563f47c6
Rosteslav Yordanov <rosteslav.yordanov@gmail.com>
2022-12-24 15:28:49 +0200

    Basic Consumer added

---
+++ RabbitMQBasicDemo/RabbitMQBasicDemo.Consumer/Consumer.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+    var consomer = new EventingBasicConsumer(channel);
+    consomer.Received += (model, ea) =>
+    {
+        var body = ea.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+        Console.WriteLine($"Recieved {message}");
+    };
+
+    channel.BasicConsume("BasicTest", true, consomer);
+}
+
+Console.ReadLine();
