929da9383670927ccea11580f50a2578ba5c6374
Rosteslav Yordanov <rosteslav.yordanov@gmail.com>
2022-12-24 15:10:27 +0200

    RabbitMQ Provider

--- RabbitMQBasicDemo/RabbitMQBasicDemo.Provider/Program.cs
+++ RabbitMQBasicDemo/RabbitMQBasicDemo.Provider/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+    var message = "getting started with RabbitMQ";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "BasicTest", null, body);
+    Console.WriteLine($"Send Message {message}");
+}
+
+Console.ReadLine();
